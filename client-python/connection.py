9b71ea4d4b3c1f5e2263405a49746123ff5659c5
Sam Weese <weesesr@mail.uc.edu>
2024-03-23 11:47:16 -0400

    added fpass on connection

--- client-python/connection.py
+++ client-python/connection.py
@@ -1,6 +1,16 @@
 # Message
     # Uses Unix Timestamp
 
+'''
+        // Message will be in following order 
+        // Action
+        // Group
+        // Message ID
+        // Sender
+        // Post Date
+        // Subject
+        // Message
+'''
 class Message:
     def __init__(self, action, group, message_id, sender, post_date, subject, message):
         self.action = action
@@ -17,17 +27,56 @@ class Message:
         return f"{self.action}|{self.group}|{self.message_id}|{self.sender}|{self.post_date}|{self.subject}|{self.message}"
 
 
-'''
-        // Message will be in following order 
-        // Action
-        // Group
-        // Message ID
-        // Sender
-        // Post Date
-        // Subject
-        // Message
-'''
 
 # Connection
+# Error courtesy of chatgpt
+import socket
+import threading
 
-# Actions
+class Connection:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._is_connected = False
+        self._receive_thread = threading.Thread(target=self._receive_loop)
+        self._receive_thread.daemon = True  # Daemonize the thread so it exits when the main thread exits
+        self._receive_thread.start()
+
+    def connect(self):
+        try:
+            self.socket.connect((self.host, self.port))
+            self._is_connected = True
+            print("Connected to", (self.host, self.port))
+        except ConnectionRefusedError:
+            print("Connection refused. Make sure the server is running and the port is correct.")
+            self.disconnect()
+
+    def disconnect(self):
+        if self._is_connected:
+            self.socket.close()
+            self._is_connected = False
+            print("Disconnected from", (self.host, self.port))
+
+    def send(self, data:Message):
+        if self._is_connected:
+            try:
+                self.socket.sendall(data.to_string.encode())
+            except BrokenPipeError:
+                print("Connection closed by peer.")
+                self.disconnect()
+
+    def _receive_loop(self):
+        while True:
+            if self._is_connected:
+                try:
+                    data = self.socket.recv(1024)
+                    if not data:
+                        print("Connection closed by peer.")
+                        self.disconnect()
+                        break
+                    print("Received:", data.decode())
+                except ConnectionResetError:
+                    print("Connection reset by peer.")
+                    self.disconnect()
+                    break
